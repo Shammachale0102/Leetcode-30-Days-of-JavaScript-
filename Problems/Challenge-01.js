400a9b744f016ecd38a1725b6673c78ddf2839dc
Sham Machale <sham.machale0102@gmail.com>
2024-12-12 11:36:52 +0530

    solve Create Hello World Function

---
+++ Problems/Challenge-01.js
@@ -0,0 +1,11 @@
+/* 1.Create Hello World Function:
+ Write a function createHelloWorld. It should return a new function that always returns "Hello World". 
+ */
+
+
+ var createHelloWorld = function() {
+    
+    return function(...args) {
+          return "Hello World";
+    }
+};
