da5f9eec017bc9fb1e373dfa2d39ad6e7413c726
Sham Machale <sham.machale0102@gmail.com>
2024-12-15 00:35:14 +0530

    solve Counter II

---
+++ Problems/Challenge-04.js
@@ -0,0 +1,21 @@
+/*Counter II:
+Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+ */
+var createCounter = function(init) {
+    let currentValue = init;
+
+    return {
+        increment: function() {
+            currentValue += 1;
+            return currentValue;
+        },
+        decrement: function() {
+            currentValue -= 1;
+            return currentValue;
+        },
+        reset: function() {
+            currentValue = init;
+            return currentValue;
+        }
+    };
+};
